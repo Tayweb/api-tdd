81e88947fa4f09903c5e2ee29a9c23b1502bead5
Tainá Fontes <tainafontes058@gmail.com>
2023-05-02 20:39:12 -0300

    Criação da classe de Usuário

---
+++ src/main/java/br/com/tayweb/api/domain/Usuario.java
@@ -0,0 +1,26 @@
+package br.com.tayweb.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+}
