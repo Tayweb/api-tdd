98493b061ede725fa26a1453ca9bf57b58f8156d
Tainá Fontes <tainafontes058@gmail.com>
2023-05-03 23:43:02 -0300

    Inserindo no retorno do metodo da classe UsuarioResource o DTO com o uso do Mapper

--- src/main/java/br/com/tayweb/api/resources/UsuarioResource.java
+++ src/main/java/br/com/tayweb/api/resources/UsuarioResource.java
@@ -1,8 +1,10 @@
 package br.com.tayweb.api.resources;
 
-import br.com.tayweb.api.domain.Usuario;
+import br.com.tayweb.api.domain.dto.UsuarioDTO;
 import br.com.tayweb.api.services.UsuarioService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,11 @@ public class UsuarioResource {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @GetMapping("/{id}")
-    public Usuario findById(@PathVariable Long id){
-        return usuarioService.findById(id);
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(mapper.map(usuarioService.findById(id), UsuarioDTO.class));
     }
 }
