d19912c0f5fbc6135a7474edeb4a5821b7bd59cc
Tainá Fontes <tainafontes058@gmail.com>
2023-06-14 01:30:59 -0300

    Retornando versão da classe UsuarioDTO

--- src/main/java/br/com/tayweb/api/domain/dto/UsuarioDTO.java
+++ src/main/java/br/com/tayweb/api/domain/dto/UsuarioDTO.java
@@ -1,10 +1,13 @@
 package br.com.tayweb.api.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UsuarioDTO {
@@ -12,4 +15,7 @@ public class UsuarioDTO {
         private Long id;
         private String nome;
         private String email;
+
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        private String senha;
 }
